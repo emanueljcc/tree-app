97c56ca714132634c4b7e7fed6705e51b4fac84f
Emanuel Castillo <emanuelcastillo9101@gmail.com>
2023-09-29 20:52:25 -0400

    first commit 🏃‍♂️

---
+++ index.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import {AppRegistry} from 'react-native';
+import {MD3LightTheme as DefaultTheme, PaperProvider} from 'react-native-paper';
+
+import {name as appName} from './app.json';
+import App from './src/App';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    /* primary: 'tomato',
+    secondary: 'yellow', */
+  },
+};
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
